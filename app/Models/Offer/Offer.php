b3d207a5c43c5414690f51423ee7444d7dd9ca8c
rahaf-jamool <rahafjamool1@gmail.com>
2022-04-06 18:44:51 -0700

    add product in offer

--- app/Models/Offer/Offer.php
+++ app/Models/Offer/Offer.php
@@ -4,6 +4,8 @@ namespace App\Models\Offer;
 
 use App\Models\Comment\Comment;
 use App\Models\Products\Product;
+use App\Models\Stores\Store;
+use App\Models\Stores\StoreProduct;
 use App\Scopes\OfferScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -40,10 +42,11 @@ class Offer extends Model
         return $this->hasMany (OfferTranslation::class,'offer_id');
     }
 
+
     public function storeProduct ()
     {
         return $this->belongsToMany (
-            Product::class,
+            StoreProduct::class,
             'store_products_offers',
             'offer_id',
             'store_product_id',
